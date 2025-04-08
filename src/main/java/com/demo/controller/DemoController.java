6c87159725bd6e1507e8299a0ad74f6ae0842a80
Sharanvthu <Sharanvthu@gmail.com>
2025-04-08 12:13:25 +0530

    demo-app

---
+++ src/main/java/com/demo/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @GetMapping("/message")
+    public String helloWorld() {
+        return "Hello, Spring Boot!";
+    }
+}
