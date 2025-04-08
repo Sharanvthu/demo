4b688a259e54c4ddf135833ccc9af77237c16b3d
Sharanvthu <Sharanvthu@gmail.com>
2025-04-08 23:51:10 +0530

    second commit

--- src/main/java/com/demo/controller/DemoController.java
+++ src/main/java/com/demo/controller/DemoController.java
@@ -4,10 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class DemoController {
+    Date date;
     @GetMapping("/message")
     public String helloWorld() {
-        return "Hello, Spring Boot!";
+        return "Hello, Spring Boot!"+date.getTime();
     }
 }
